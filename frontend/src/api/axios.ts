a25e5568853b4dbcbbc1b183fc1bd9dcccfa16b3
Miguefj00 <48717614+Miguefj00@users.noreply.github.com>
2026-07-07 16:10:55 +0200

    Preparing frontend for Docker

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,20 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
-
-    const token =
-        localStorage.getItem("token");
-
-    console.log("TOKEN ENVIADO:", token);
+    const token = localStorage.getItem("token");
 
     if (token) {
-
-        config.headers.Authorization =
-            `Bearer ${token}`;
+        config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
